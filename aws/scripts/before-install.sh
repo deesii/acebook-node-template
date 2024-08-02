b6c0ef4699b302848a0994414168af4c063eb3e7
Donna Cheng <donnagph@hotmail.com>
2024-08-02 10:42:32 +0100

    before install script updated

---
+++ aws/scripts/before-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+# Delete the old  directory as needed.
+if [ -d /usr/local/codedeployresources ]; then
+    rm -rf /usr/local/codedeployresources/
+fi
+
+mkdir -vp /usr/local/codedeployresources
+
+# unzip bundle 
+
+unzip -d /usr/local/codedeployresources/ {md-acebook.zip}
