bae0c0faf3216e3385b6d8983aaa1fbcb2bfa32d
Donna Cheng <donnagph@hotmail.com>
2024-08-02 12:08:35 +0100

    user changed

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Set ownership to www-data (common user for web servers)
-chown -R www-data:www-data /var/www/myapp
+chown -R ec2-user /var/www/myapp
 
 # Set permissions: owner can read/write/execute, group can read/execute, others can read/execute
 chmod -R 755 /var/www/myapp
