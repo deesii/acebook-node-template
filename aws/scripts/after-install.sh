4ceaa2ed9dcedc30b9378ce010a929b01960f1a3
Donna Cheng <donnagph@hotmail.com>
2024-08-02 12:45:19 +0100

    update script

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Set ownership to ec2-user
-chown -R ec2-userec2-user /var/www/myapp
+chown -R ec2-user:ec2-user /var/www/myapp
 
 # Set permissions: owner can read/write/execute, group can read/execute, others can read/execute
 chmod -R 755 /var/www/myapp
