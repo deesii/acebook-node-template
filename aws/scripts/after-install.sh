d7db55d7aea1a87757979e4453dad0c271bd944f
Donna Cheng <donnagph@hotmail.com>
2024-08-02 12:26:27 +0100

    removed application stop

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xe
 
-# Set ownership to www-data (common user for web servers)
-chown -R ec2-user /var/www/myapp
+# Set ownership to ec2-user
+chown -R ec2-userec2-user /var/www/myapp
 
 # Set permissions: owner can read/write/execute, group can read/execute, others can read/execute
 chmod -R 755 /var/www/myapp
--- aws/scripts/application-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set variables
-reponame=myapp
-pm2_proc=$reponame
-
-# Stop application
-pm2 stop $pm2_proc
-pm2 delete $pm2_proc
